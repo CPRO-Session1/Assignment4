fa5d09291f01d38aa151a51ee2afefd54cf4307f
Skee <skee66499@Gmail.com>
2016-07-05 00:38:45 -0400

    v1.3 Added ASCII Art

--- hangman.c
+++ hangman.c
@@ -1,11 +1,12 @@
 /*Sean Kee*/
-/*Hangman v1.1*/
+/*Hangman v1.3*/
 #include <stdio.h>
 
 int main()
 {
 	printf("Welcome to HANGMAN\n");
-	printf("******************\n\n");
+	printf("******************\n");
+	printf("   ______\n   I    !\n   I   \\@/\n   I    |\n   I   / \\     __@\n   I  [&&&]   \\  |\\\n[__I__[&&&]___II/ \\__\n| HANGMAN  v1.3      |__\n|_______________SEAN KEE|\n\n\n");
 	
 	char display[6];
 	char word[6] = "rhythm";
@@ -24,7 +25,7 @@ int main()
 	do
 	{
 		//Resets all variables before game starts
-		tries = 6;
+		tries = 7;
 		for (i = 0; i < 6; i++)
 			display[i] = '_';
 		for (i = 0; i < 26; i++)
@@ -35,11 +36,57 @@ int main()
 		{
 			while (tries > 0)
 			{
+				printf("\n\n");
+				if (result == 0)
+					tries == 8;
+				switch(tries)
+				{
+					case 1:
+						{
+							printf("   ______\n   I    !\n   I   \\@/\n   I    |\n   I   / \\     __@\n   I  [&&&]   \\  |\\\n[__I__[&&&]___II/ \\__\n| Tries Left: %d     |__\n|_______________________|\n", tries);
+							break;
+						}
+					case 2:
+						{
+							printf("   ______\n   I    !\n   I   \\@\n   I    |\\\n   I   /       __@\n   I  [&&&]   \\  |\\\n[__I__[&&&]___II/ \\__\n| Tries Left: %d     |__\n|_______________________|\n", tries);
+							break;
+						}
+					case 3:
+						{
+							printf("   ______\n   I    !\n   I    @\n   I   /|\\\n   I           __@\n   I  [&&&]   \\  |\\\n[__I__[&&&]___II/ \\__\n| Tries Left: %d     |__\n|_______________________|\n", tries);
+							break;
+						}
+					case 4:
+						{
+							printf("   ______\n   I    !\n   I    @\n   I   /|\n   I           __@\n   I  [&&&]   \\  |\\\n[__I__[&&&]___II/ \\__\n| Tries Left: %d     |__\n|_______________________|\n", tries);
+							break;
+						}
+					case 5:
+						{
+							printf("   ______\n   I    !\n   I    @\n   I    |\n   I           __@\n   I  [&&&]   \\  |\\\n[__I__[&&&]___II/ \\__\n| Tries Left: %d     |__\n|_______________________|\n", tries);
+							break;
+						}
+					case 6:
+						{
+							printf("   ______\n   I    !\n   I    @\n   I\n   I           __@\n   I  [&&&]   \\  |\\\n[__I__[&&&]___II/ \\__\n| Tries Left: %d     |__\n|_______________________|\n", tries);
+							break;
+						}
+					case 7:
+						{
+							printf("   ______\n   I    !\n   I\n   I\n   I           __@\n   I  [&&&]   \\  |\\\n[__I__[&&&]___II/ \\__\n| Tries Left: %d     |__\n|_______________________|\n", tries);
+							break;
+						}
+					case 8:
+						{
+							printf("   ______\n   I    !\n   I\n   I\n   I   \\@/     __@\n   I    |     \\  |\\\n[__I___/ \\____II/ \\__\n| Congrats! You won! |__\n|_______________________|\n");
+							break;
+						}
+				}
+				printf("\n");
 				for (i = 0; i < 6; i++)
 				{
 					printf("%c ", display[i]);
 				}
-				printf("\nTries Remaining: %d", tries);
 				printf("\nUsed Letter: ");
 				for (i = 0; i < 26; i++)
 				{
@@ -88,12 +135,9 @@ int main()
 					else
 						result = 0;
 				}
-
 			}
-			if (result == 0)
-				printf("\n\nCongrats! You won!\n\n");
-			else
-				printf("\n\nAwe shucks. You lost :\(\n\n");
+			if (result == 1)
+				printf("\n\n   ______\n   I    !\n   I    !\n   I    !\n   I    !       __@\n   I    !      /  |\\\n[__I__  !  ___II_/ \\__\n| You | ! | Failed.  |__\n|_____| ! |_____________|\n\n");
 		}
 		printf("Would you like to play again? [Y:1/N:2]\n#: ");
 		scanf("%d", &startGame);
