ed5626cda9537eeb0b0ad36fd653c939d9b894d4
Lloyd Page <lloyd.page@gmail.com>
2016-07-05 10:18:35 -0400

    commit Hangman.c

--- Hangman.c
+++ Hangman.c
@@ -66,7 +66,7 @@ int main()
 		{
 			printf("Enter your word/phrase\n");
 			fgets(word,sizeof(word),stdin);
-			word[strlen(word)]='\0';
+			word[strlen(word)-1]='\0';
 		}
 		char out[strlen(word)];
 		for(int i=0;i<sizeof(word);i++)
