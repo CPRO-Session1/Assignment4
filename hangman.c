6842f6e260324afb00e416138554f9071cc6c99f
Skee <skee66499@Gmail.com>
2016-07-05 11:01:18 -0400

    Added comments for easier grading

--- hangman.c
+++ hangman.c
@@ -34,15 +34,18 @@ int main()
 		for (i = 0; i < 26; i++)
 			usedLetters[i] = '1';
 		result = 1;
+		//Random number generator to pick word
 		srand(time(NULL));
 		j = rand();
 		j = (j % 10);
+		//Determines length of the word
 		for (i = 0; i < 10; i++)
 		{
 			if (words[j][i] == ' ')
 				break;
 			l++;
 		}
+		//Initializes the display and adds spaces for the amount of characters in the word
 		char display[l];
 		for (i = 0; i < l; i++)
 			display[i] = '_';
@@ -54,6 +57,7 @@ int main()
 				printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 				if (result == 0)
 					tries = 8;
+				//Displays correct graphic based on number of tries
 				switch(tries)
 				{
 					case 1:
@@ -98,32 +102,32 @@ int main()
 						}
 				}
 				printf("\n");
-				for (i = 0; i < l; i++)
+				for (i = 0; i < l; i++) //Prints out the spaces for display
 				{
 					printf("%c ", display[i]);
 				}
 				printf("\n");
 				printf("\nUsed Letter: ");
-				for (i = 0; i < 26; i++)
+				for (i = 0; i < 26; i++) //Prints out all the used letters
 				{
 					if (usedLetters[i] == '1')
 						break;
 					printf("%c|", usedLetters[i]);
 				}
-				if (result == 0)
+				if (result == 0) //stops the loop if player won
 					break;
 				printf("\n#: ");
 				input = getchar();
 				input = getchar();
-				correctLetter = 1;
-				for (i = 0; i < l; i++)
+				correctLetter = 1; //resets correctLetter to false
+				for (i = 0; i < l; i++) //tests if the inputted character is in the word
 				{
 					if(input != words[j][i])
 						continue;
 					display[i] = words[j][i];
 					correctLetter = 0;
 				}
-				if (correctLetter == 1)
+				if (correctLetter == 1) //if correctLetter is still false, subtracts tries and adds to the used characters list.
 				{
 					for (i = 0; i < 26; i++)
 					{
@@ -141,7 +145,7 @@ int main()
 							}
 					}
 				}
-				for (i = 0; i < l; i++)
+				for (i = 0; i < l; i++) //Tests to see if word is complete
 				{
 					if (words[j][i] != display[i])
 					{
@@ -152,7 +156,7 @@ int main()
 						result = 0;
 				}
 			}
-			if (result == 1)
+			if (result == 1) //Prints graphic if game is lost
 			{
 				printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 				printf("\n\n   ______\n   I    !\n   I    !\n   I    !\n   I    !       __@\n   I    !      /  |\\\n[__I__  !  ___II_/ \\__\n| You | ! | Failed.  |__\n|_____| ! |_____________|\n\n\n");
